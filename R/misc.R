878424172f15a3462939a31ed2a14a2107ff988f
Sharad Purohit <Spurohit@augusta.edu>
2026-08-24 16:12:01 -0400

    Update misc.R
    made the script more customizable by changing how the data is looked into the individual lists.
    changed color of the lines to gray
    turnoff the percentage labels for visual appeal
    added legend

--- R/misc.R
+++ R/misc.R
@@ -49,51 +49,115 @@ outliers_to_na<-function(iggdata){
 #' 
 #' @param daisy igg data
 #' @param gly_class glycan class to assess. Default = "Aminoglycoside antibiotics"
+#' @param class_col glycan grouping columns for your proteins, genes, glycans eg class_col = "Glycan.Class" or "group"
+#' @param id_col name of molecules stored in  a separate file . eg  default is id_col = "Glycan.ID"
+#' @param group_col grouping variable. Default is group_col = "Group" values are (c("Control", "Non-progressor", "Progressor")
+#' @param group_levels order of levels in your grouping column default is group_levels = c("Control", "Non-progressor", "Progressor")
+#' @param scale values are TRUE or FALSE, if you supplied scaled data then change it to FALSE. default is scale=TRUE
+#' @param legend_labels comming from your grouping column default is group_levels = c("Control", "Non-progressor", "Progressor")
+
 #' 
 #' @return spider plot
 #' @export
-univariate_radar_chart<-function(daisy=daisy,gly_class="Aminoglycoside antibiotics",label=T){
+univariate_radar_chart <- function(daisy = daisy, 
+                                   covariates = c("Group", "Sex",  "Draw_Age"),
+                                   gly_class = "Aminoglycoside antibiotics", 
+                                   label = TRUE,
+                                   class_col = "Glycan.Class", 
+                                   id_col = "Glycan.ID",
+                                   glygroups = glycan_classes,
+                                   group_col = "Group",
+                                   group_levels = c("Control", "Non-progressor", "Progressor"),
+                                  scale=TRUE,
+                                  legend_labels = c("Control", "Non-progressor", "Progressor")) {
   
-  aminoglycosides<-glycan_classes$Glycan.ID[glycan_classes$Glycan.Class==gly_class]
-  aminoglycosides<-aminoglycosides[!is.na(aminoglycosides)]
-  aminoglycosides<-intersect(aminoglycosides,colnames(daisy$igg))
-  daisy$combined<-cbind.data.frame(apply(daisy$igg[,aminoglycosides],2,scale),daisy$pheno)
-  daisy$combined$Group<-factor(daisy$combined$Group,levels = c("Control","Non-progressor","Progressor"))
-  daisy_aminoglyc_models<-univariate_models(daisy$combined,
-                                            c("Group", 
-                                              "Sex", "FDR", 
-                                              "HLA_risk","Draw_Age"))
+  # Dynamically extract classes and IDs using custom column names and trim whitespace
+  classes_vec <- trimws(as.character(glygroups[[class_col]]))
+  ids_vec <- trimws(as.character(glygroups[[id_col]]))
   
-  radar_df<-rbind.data.frame(1.5,0,
-                             -log10(daisy_aminoglyc_models$small_table$prog_p),
-                             -log10(0.05),
-                             -log10(daisy_aminoglyc_models$small_table$nonprog_p)
-                             
-                             # daisy_aminoglyc_models$small_table$nonprog_est)
+  # Filter IDs matching the requested gly_class
+  selected_ids <- ids_vec[classes_vec == trimws(gly_class)]
+  selected_ids <- selected_ids[!is.na(selected_ids)]
+  selected_ids <- intersect(selected_ids, colnames(daisy$igg))
+  
+  # Safety check: Stop and print available classes if none match
+  if (length(selected_ids) == 0) {
+    stop(paste("No matching molecules found for group:", gly_class, 
+               "\nAvailable groups are:", paste(unique(classes_vec), collapse = ", ")))
+  }
+  
+  if (scale == TRUE) {
+    daisy$combined <- cbind.data.frame(
+      apply(daisy$igg[, selected_ids, drop = FALSE], 2, scale), 
+      daisy$pheno
+    )
+  } else {
+    daisy$combined <- cbind.data.frame(
+      daisy$igg[, selected_ids, drop = FALSE], 
+      daisy$pheno
+    )
+  }
+  
+  # 4. Create/overwrite the Group column using your specific phenotype column (e.g., ptype)
+  # Replace 'ptype' with whatever column name exists in daisy$pheno if it changes
+  daisy$combined$Group <- factor(
+    daisy$pheno[[group_col]], 
+    levels = group_levels
   )
-  colnames(radar_df)<-rownames(daisy_aminoglyc_models$small_table)
-  areas <- c(rgb(0, 0, 1, 0.15),rgb(0, 0, 0, 0.1),rgb(1, 0, 0, 0.15))
-  if (label==F){
-    fmsb::radarchart(radar_df,
-               pfcol = areas,
-               pty = c(16,32,16),
-               pcol = c("blue","black","red"),
-               axistype = 0,
-               # caxislabels = c("-log10(p-value)",NA,NA,"0.05"),
-               calcex = 1,
-               vlabels = NA,
-               axislabcol = "black")
+  
+  daisy_aminoglyc_models <- univariate_models(
+    daisy$combined,
+    covariates, group_col = group_col,
+    depVarList=selected_ids
+      )
+  
+  radar_df <- rbind.data.frame(
+    1.5, 0,
+    -log10(daisy_aminoglyc_models$small_table$prog_p),
+    -log10(0.05),
+    -log10(daisy_aminoglyc_models$small_table$nonprog_p)
+  )
+  
+  colnames(radar_df) <- rownames(daisy_aminoglyc_models$small_table)
+  # 2. Dynamically calculate the maximum value in your data and add 10% padding
+  max_val <- ceiling(max(radar_df, na.rm = TRUE) * 1.1)
+  
+  # Ensure the max ceiling is at least 1.5 just in case all p-values are very high/low
+  max_val <- max(max_val, 1.5) 
+  
+  # 3. Prepend the Max (row 1) and Min (row 2) rows required by fmsb::radarchart
+  radar_df <- rbind(
+    rep(max_val, ncol(radar_df)), # Row 1: Dynamic Maximum Ceiling
+    rep(0, ncol(radar_df)),       # Row 2: Minimum (0)
+    radar_df                      # Rows 3+: Your actual data
+  )
+  
+  areas <- c(rgb(0, 0, 1, 0.15), rgb(0, 0, 0, 0.1), rgb(1, 0, 0, 0.15))
+  
+  if (label == FALSE) {
+    fmsb::radarchart(
+      radar_df, pfcol = areas, pty = c(16, 32, 16),
+      pcol = c("blue", "black", "red"), axistype = 0, # Changed axistype to 1 so you can see scale rings
+      calcex = 1, vlabels = NA, axislabcol = "black",
+      cglcol = "gray",
+    )
+  } else {
+    fmsb::radarchart(
+      radar_df, pfcol = areas, pty = c(16, 32, 16),
+      pcol = c("blue", "black", "red"), axistype = 0, # Changed axistype to 1
+      calcex = 1, axislabcol = "black",
+      cglcol = "gray",
+    )
   }
-  else {
-    fmsb::radarchart(radar_df,
-               pfcol = areas,
-               pty = c(16,32,16),
-               pcol = c("blue","black","red"),
-               axistype = 0,
-               # caxislabels = c("-log10(p-value)",NA,NA,"0.05"),
-               calcex = 1,
-               axislabcol = "black")
-  }
+  # Add the Legend
+  legend(
+    x = "topright",                          # Position of the legend (e.g., "topright", "bottomleft", etc.)
+    legend = legend_labels, # Labels matching your groups
+    col = c("blue", "black", "red"),         # Line colors matching pcol
+    lty = 1,                                 # Line type
+    lwd = 2,                                 # Line width
+    bty = "n"                                # Box type ("n" removes the border box)
+  )
 }
 
 
